40e25f333a02b71231e153259489b53c321070ce
saiddlkc <saidalkilic@gmail.com>
2024-08-19 15:36:41 +0200

    navbar

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+import "./App.css";
+import "./index.css";
+
+import NavbarComp from "./Components/NavbarComp";
+
+function App() {
+  return (
+    <>
+      <NavbarComp />
+    </>
+  );
+}
+
+export default App;
